23f0975868ec1334cebf524f23886e04db65c508
Your Name <ti.rafaelteles@gmail.com>
2023-01-07 17:30:59 -0300

    filtro

--- src/Controller/Persistencia.php
+++ src/Controller/Persistencia.php
@@ -18,8 +18,13 @@ class Persistencia implements InterfaceControladorRequisicao
 
     public function processaRequisicao(): void
     {
+        $descricao = filter_input(
+            INPUT_POST,                                                                 /* De onde estão vindo os dados que devem ser filtrados */
+            'descricao',
+            FILTER_SANITIZE_STRING);                                                    /* Para garantir que ela possua somente caracteres validos */ 
+
         $curso = new Curso();
-        $curso->setDescricao($_POST['descricao']);
+        $curso->setDescricao($descricao);
 
         $this->entityManager->persist($curso);
         $this->entityManager->flush();
